da32bda9b87da2814b7a21d0297faf530fea26c9
anon <yours@truly.xyz>
2023-03-10 17:56:25 +0100

    Add types.h

---
+++ include/types.hh
@@ -0,0 +1,14 @@
+
+#pragma once
+
+#include <stddef.h>
+
+using uint8_t = unsigned char;
+using uint16_t = unsigned short;
+using uint32_t = unsigned long;
+using uint64_t = unsigned long long;
+
+using int8_t = char;
+using int16_t = short;
+using int32_t = long;
+using int64_t = long long;
