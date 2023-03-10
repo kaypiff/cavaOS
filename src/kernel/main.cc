42e21b97c8d6be977197bbca68fcb7cdaad976d5
anon <yours@truly.xyz>
2023-03-10 17:57:26 +0100

    Call global constructors

--- src/kernel/main.cc
+++ src/kernel/main.cc
@@ -1,5 +1,15 @@
+/* Call global C++ constructors */
+extern "C" void (*ctors_begin)(void);
+extern "C" void (*ctors_end)(void);
+void call_constructors()
+{
+    for (auto ctor = ctors_begin; ctor != ctors_end; ctor++)
+        ctor();
+}
+
 
 extern "C" void kernel_main()
 {
+    call_constructors();
     for (;;);
 }
